b0f936c6eb14ec75815cac6e9a4208ae8ab2618a
Christopher A. Brown <christopherabrown42@gmail.com>
2019-07-19 15:53:16 -0400

    Update paste_tax()
    Adjustment of naming conventions and tweaks to documentation

--- R/paste_tax.R
+++ R/paste_tax.R
@@ -1,13 +1,13 @@
 #' Paste Genus information onto a vector of OTU numbers.
 #'
-#' @param otu_vec Vector of OTU numbers, used to select rownames (OTUs) in taxonomy table.
-#' @param tax Taxonomy table. Default is otu.good.taxonomy (used historically).
-#' @return A factor vector comprised of taxonomy label and OTU number, with levels that match the order of OTUs in \code{otu_vec}.
+#' @param otu_vec Vector of OTU identifiers, used to select rownames (OTU IDs) in \code{tax}.
+#' @param tax Taxonomy table, otu_good_taxonomy by default.
+#' @return A factor vector comprised of taxonomy label and OTU identifier, with levels that match the order of OTUs in \code{otu_vec}.
 #' @export
 #' @examples
-#' paste_tax(rownames(otu.good.taxonomy)[1:20])
-#' otu.df$OTU <- paste_tax(otu.df$OTU)
+#' paste_tax(rownames(otu_good_taxonomy)[1:20])
+#' otu_df$OTU <- paste_tax(otu_df$OTU)
 
-paste_tax <- function(otu_vec, tax = otu.good.taxonomy) {
+paste_tax <- function(otu_vec, tax = otu_good_taxonomy) {
   forcats::fct_inorder(paste0(tax[otu_vec, ]$Genus, " (", otu_vec, ")"))
 }
