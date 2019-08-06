8983fd6a4a88b327934ad86e4f75751acf50ceec
Christopher A. Brown <christopherabrown42@gmail.com>
2019-08-06 11:13:15 -0400

    Add many_rf()
    many_rf() creates a list of random forests using randomForest() and other parameters.

---
+++ R/many_rf.R
@@ -0,0 +1,20 @@
+#' Generate a list of randomForest models.
+#'
+#' @param df Dataframe that contains the variables to be included in the model.
+#' @param y The response vector.
+#' @param nforest Number of random forests to create, 100 by default.
+#' @param seed Value passed to \code{set.seed()}, for ensuring reproducibility. Although the default is 1234, best practice would be to vary the seed, perhaps by project, or even by model.
+#' @param ... Optional parameters to pass to \code{randomForest()}.
+#' @return A list object containing \code{nforest randomForest} models.
+#' @export
+#' @examples
+#' many_rf(data = vfd_fam.norm[, -length(vfd_fam.norm)], y = vfd_fam.norm[, "VFDandAlive"], importance = TRUE)
+
+many_rf <- function(df, y, nforest = 100, seed = 1234, ...) {
+  set.seed(seed)
+  rflist <- vector("list", nforest)  # creating list vector ahead of time is much faster than during loop
+  for (i in seq_along(1:nforest)) {
+    rflist[[i]] <- randomForest::randomForest(y ~ ., data = df, ...)
+  }
+  return(rflist)
+}
