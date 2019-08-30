d7f34af94ad57af0b0f8d14fd1b816015fd93ce1
Christopher A. Brown <christopherabrown42@gmail.com>
2019-08-30 12:23:06 -0400

    Update plot_ra()
    Added support for points layer representing individual observations, and also for plotting any taxonomic level present in supplied dataframe(s) (rather than just OTU).

--- R/plot_ra.R
+++ R/plot_ra.R
@@ -1,28 +1,42 @@
 #' Create an ordered relative abundance plot
 #'
-#' @param df Dataframe containing metadata and OTU abundance data in 'long' format, e.g. using tidyr::gather().
+#' @param df Dataframe containing metadata and taxon (Phylum, Otu, etc) abundance data in 'long' format, e.g. using tidyr::gather().
+#' @param df_obs An optional dataframe containing individual observations, when \code{df} contains summarized samples. \code{df_obs} will be used to add a points layer.
+#' @param taxon Which taxonomic level within \code{df} (and optionally, \code{df_obs}) should be plotted?
 #' @param title String to use as plot title. Defaults to an empty string.
 #' @param error_bar Boolean for whether to include an errorbar. If so, \code{df} should contain a column named SEM with this information.
 #' @param facet_var Variable to use for faceted plots. By default, no faceting will be used.
 #' @param tax_fill Variable to use for fill color based on taxonomy table. By default, fill color is not based on taxonomy.
 #' @param gtxt Boolean for whether to include a geom_text() layer in the plot which will display actual percentages. Not included by default.
+#' @param seed Integer to be used as a random seed to ensure reproducibility. For example, this would come into play if using \code{df_obs} which will create a \code{geom_jitter()} layer that adds random noise to points.
 #' @return A plot created based on \code{df} and the specified parameters.
 #' @export
 #' @examples
-#' plot_ra(mock.df, title = "Cross-Comparison of Mock Control Replicates", facet_var = "Sample", tax_fill = "Phylum", txt = T)
-#' plot_ra(samp.df, title = "Exp1 & 2 Stomachs by Treatment, Ordered by Untreated", facet_var = "Group", tax_fill = "Phylum", error_bar = T)
+#' plot_ra(mock_df, title = "Cross-Comparison of Mock Control Replicates", facet_var = "Sample",
+#'    tax_fill = "Phylum", txt = T)
+#'
+#' plot_ra(samp_df, title = "Exp1 & 2 Stomachs by Treatment, Ordered by Untreated",
+#'    facet_var = "Group", tax_fill = "Phylum", error_bar = T)
+#'
+#' plot_ra(df = otu_agg, df_obs = otu_obs, title = "Feces Samples, Ordered by Day 0",
+#'    facet_var = "Day", tax_fill = "Phylum")
+#'
+#' plot_ra(df = phy_agg, df_obs = phy_obs, taxon = "Phylum",
+#'    title = "Feces Samples, Aggregated by Phylum, Ordered by Day 0",
+#'    facet_var = "Day", tax_fill = "Phylum")
 
 # work in progress; now supports NULL case for tax_fill and as well as NULL/non-NULL faceting
-plot_ra <- function(df, title = "", error_bar = FALSE, facet_var = NULL, tax_fill = NULL, gtxt = FALSE) {
+plot_ra <- function(df, df_obs = NULL, taxon = "OTU", title = "", error_bar = FALSE, facet_var = NULL, tax_fill = NULL, gtxt = FALSE, seed = 123) {
+  set.seed(seed)
 
   # account for fill, either by a taxonomy condition, or grey
   if(!is.null(tax_fill)) { # controls all tax cases, and Sample (controls) (or presumably other columns that exist in data)
-    p <- ggplot(data = df, aes(y = Mean_Perc, x = OTU, fill = .data[[tax_fill]])) +
-      geom_col() +
+    p <- ggplot(data = df, aes(y = Mean_Perc, x = .data[[taxon]])) +
+      geom_col(aes(fill = .data[[tax_fill]])) +
       labs(fill = tax_fill)
   } # should fill be set to facet_var in cases where tax_fill (e.g. taxonomy data) is null?
   else { # This is primarily aimed at cases where there is no tax_fill or faceting
-    p <- ggplot(data = df, aes(y = Mean_Perc, x = OTU)) +
+    p <- ggplot(data = df, aes(y = Mean_Perc, x = .data[[taxon]])) +
       geom_col(fill="grey")
   }
 
@@ -33,15 +47,27 @@ plot_ra <- function(df, title = "", error_bar = FALSE, facet_var = NULL, tax_fil
     p <- p + facet_grid(rows = vars(.data[[facet_var]]))
   }
 
-  # add error bar if requested (or if column exists?)
+  # add error bar if requested
   if(error_bar) {
     p <- p + geom_errorbar(aes(ymin = Mean_Perc - SEM, ymax = Mean_Perc + SEM, width = 0.3))
   }
 
+  if (!is.null(df_obs)) { # If a dataframe of observations is provided, assume it should be added as a points layer
+    p <- p + geom_jitter(data = df_obs, aes(y = Percentage, x = .data[[taxon]]), alpha = 0.3, width = .15)
+  }
+
+  # Determine x-axis label
+  xlabel <- case_when(stringr::str_detect(taxon, "OTU") ~ "OTUs",
+                      stringr::str_detect(taxon, "Phylum") ~ "Phyla",
+                      stringr::str_detect(taxon, "Class") ~ "Classes",
+                      stringr::str_detect(taxon, "Order") ~ "Orders",
+                      stringr::str_detect(taxon, "Family") ~ "Families",
+                      stringr::str_detect(taxon, "Genus") ~ "Genera")
+
   p <- p + theme_bw() + # added due to pdf knitting losing global ggtheme
     scale_y_continuous(labels = dollar_format(suffix = "%", prefix = "")) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5), legend.position = "top") + # angles axis text; repositions legend
-    labs(title = title, x = "OTUs", y = "% Relative Abundance")
+    labs(title = title, x = xlabel, y = "% Relative Abundance")
 
   if(gtxt == TRUE) {
     # this helps show OTUs have a value of 0; related to a knit issue that shows sliver of color when OTUs actually have a true value of 0
