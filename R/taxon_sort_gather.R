a3dfa85d5d654423a44200aa1422b2c1c601e93a
Christopher Brown <christopherabrown42@gmail.com>
2019-09-12 16:43:49 -0400

    Update taxon_sort_gather()

--- R/taxon_sort_gather.R
+++ R/taxon_sort_gather.R
@@ -1,12 +1,12 @@
 #' Transform data from wide to long, join a taxonomic level, aggregate and create summary statistics, and return n OTUs.
 #'
 #' @param df Dataframe containing metadata and taxon (Phylum, Otu, etc) abundance data in wide format.
-#' @param n Number of OTUs to return.
+#' @param n Number of OTUs to return, 50 by default.
 #' @param facet_var Variable to be used for grouping and eventual faceting, possibly in \code{plot_ra}.
 #' @param ord_val Level with the \code{facet_var} to be used for ranking OTUs.
 #' @param tax_df Taxonomy table. Passed to \code{\link{join_tax}}. Defaults to \code{otu_good_taxonomy}.
 #' @param tax_level Taxonomic level to join from \code{tax_df}. Defaults to Phylum.
-#' @return A factor vector comprised of taxonomy label and OTU identifier, with levels that match the order of OTUs in \code{otu_vec}.
+#' @return A dataframe that has been aggregated and reshaped into long form.
 #' @export
 #' @examples
 #' filt_ord_facet_df <- dplyr::filter(otu_good_14d, Day == 0, Organ == "Feces") %>%
@@ -30,18 +30,18 @@ taxon_sort_gather <- function(df, n = 50, facet_var = NULL, ord_val = NULL, tax_
     if(!is.null(ord_val)) { # OTUs should be ordered according to rank within specified facet level
       ord_df <- dplyr::filter(df, !!fv == ord_val)
       otu_order <- rank_otus(ord_df)
-      df <- data.frame(select_if(df, !(str_detect(colnames(otu_df), "Otu"))), df[,otu_order[1:n]]) %>%
+      df <- data.frame(dplyr::select_if(df, !(stringr::str_detect(colnames(otu_df), "Otu"))), df[,otu_order[1:n]]) %>%
         join_tax(tax_levels = tax_level, tax_df = tax_df) %>%
         dplyr::group_by(OTU, !!tl, !!fv)
     } else { # Facets, ordered by overall mean
       otu_order <- rank_otus(df)
-      df <- data.frame(select_if(df, !(str_detect(colnames(otu_df), "Otu"))), df[,otu_order[1:n]]) %>%
+      df <- data.frame(dplyr::select_if(df, !(stringr::str_detect(colnames(otu_df), "Otu"))), df[,otu_order[1:n]]) %>%
         join_tax(tax_levels = tax_level, tax_df = tax_df) %>%
         dplyr::group_by(OTU, !!tl, !!fv)
     }
   } else { # Not facets, ordered by overall mean
     otu_order <- rank_otus(df)
-    df <- data.frame(select_if(df, !(str_detect(colnames(otu_df), "Otu"))), df[,otu_order[1:n]]) %>%
+    df <- data.frame(dplyr::select_if(df, !(stringr::str_detect(colnames(otu_df), "Otu"))), df[,otu_order[1:n]]) %>%
       join_tax(tax_levels = tax_level, tax_df = tax_df) %>%
       dplyr::group_by(OTU, !!tl)
     # df <- dplyr::top_n(df, n, Mean_Perc)
