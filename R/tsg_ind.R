33795940860c9dacb8ba3f921e054616cb862c4a
Christopher Brown <christopherabrown42@gmail.com>
2019-09-13 15:11:51 -0400

    Update tsg_ind()
    Fix for OTU mismatch against aggregated df from taxon_sort_gather()

--- R/tsg_ind.R
+++ R/tsg_ind.R
@@ -4,6 +4,7 @@
 #' @param n Number of OTUs to return, 50 by default.
 #' @param ar_mean Boolean to indicate whether the arithmetic mean should be appended to the data.
 #' @param geo_mean Boolean to indicate whether the geometric mean should be appended to the data.
+#' @param otu_vec Character or factor vector of OTU identifiers. These are OTUs resulting from a dataframe output by \code{\link{taxon_sort_gather}}.
 #' @return An unaggregated dataframe that has been reshaped into long form.
 #' @seealso \code{\link{taxon_sort_gather}}
 #' @export
@@ -15,7 +16,7 @@
 #'     tsg_ind()
 
 # work in progress
-tsg_ind <- function(df, n = 50, ar_mean = FALSE, geo_mean = FALSE) {
+tsg_ind <- function(df, n = 50, ar_mean = FALSE, geo_mean = FALSE, otu_vec = NULL) {
   # Geometric mean helper: https://stackoverflow.com/questions/2602583/geometric-mean-is-there-a-built-in
   tsg_geo <- function(x, na.rm = TRUE) {
     exp(sum(log(x[x > 0]), na.rm = na.rm) / length(x))
@@ -48,7 +49,14 @@ tsg_ind <- function(df, n = 50, ar_mean = FALSE, geo_mean = FALSE) {
     temp <- temp[colMeans(temp) > 0] # remove 0-valued OTUs - could be determined by limiting rank_otus to colMeans() > 0
     df <- cbind(Sample_name = forcats::fct_inorder(sample_names), temp) # maintains rows in the original order, with means appended to end
   } else { # non-aggregation case
-    df <- dplyr::select(df, stringr::str_which(colnames(df), "Otu", negate = TRUE), otu_order[1:n]) # Currently does not trim 0-valued OTUs
+    if(!is.null(otu_vec)) {
+      if(class(otu_vec)=="factor") {
+        otu_vec <- levels(otu_vec)
+      } # otherwise a character vector is expected
+      df <- dplyr::select(df, stringr::str_which(colnames(df), "Otu", negate = TRUE), stringr::str_extract(otu_vec, "Otu\\d+")) # Retains OTUs in order from otu_vec
+    } else { # retain all OTUs, ordered by in-data means
+      df <- dplyr::select(df, stringr::str_which(colnames(df), "Otu", negate = TRUE), otu_order[1:n]) # Currently does not trim 0-valued OTUs
+    }
   }
 
   # reformats the otu data from wide to long, must include factor_key for factor relabeling
