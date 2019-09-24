3189a2115d5b1c9d2c8fbc1520efab9ca5a2e8cf
Christopher Brown <christopherabrown42@gmail.com>
2019-09-24 08:31:23 -0400

    Update load_shared()
    Added a parameter for setting the trimming threshold.

--- R/load_shared.R
+++ R/load_shared.R
@@ -1,12 +1,13 @@
 #' Reads in a file containing OTU count data and keeps only OTUs > 0.1 \% of the population.
 #'
 #' @param shared An opti_mcc.shared file resulting from processing via mothur.
+#' @param thresh Threshold used to determine whether any OTUs should be trimmed. Defaults to 0.1.
 #' @return Returns a matrix containing OTU counts, trimmed to remove OTUs < 0.1 \% of the population.
 #' @export
 #' @examples
 #' load_shared("miseq.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.opti_mcc.shared")
 
-load_shared <- function(shared) {
+load_shared <- function(shared, thresh = 0.1) {
   # Read in OTU counts
   otu_raw <- read.table(shared, row.names = 2, header = T)
 
@@ -16,11 +17,7 @@ load_shared <- function(shared) {
   # Convert trim to matrix in order to work with which() since data.frame will throw an error
   otu_trim <- as.matrix(otu_trim)
   otu_tmp <- vegan::decostand(otu_trim, "total") * 100
-  otu_trim[which(otu_tmp < 0.1)] <- 0
+  otu_trim[which(otu_tmp < thresh)] <- 0
   otu_good <- otu_trim[, which(colSums(otu_trim) > 0)]
   return(otu_good)
 }
-
-
-
-
