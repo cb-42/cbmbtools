edbd5dc6f1e163c10cad9f13f4c3714be1c5cfbb
Christopher Brown <christopherabrown42@gmail.com>
2019-09-24 11:11:14 -0400

    Update load_shared()
    Added an optional parameter for excluding additional OTUs prior to trimming.

--- R/load_shared.R
+++ R/load_shared.R
@@ -1,18 +1,24 @@
-#' Reads in a file containing OTU count data and keeps only OTUs > 0.1 \% of the population.
+#' Reads in a file containing OTU count data and keeps only OTUs present above a specified \% of the population in at least one sample.
 #'
 #' @param shared An opti_mcc.shared file resulting from processing via mothur.
+#' @param otu_vec An optional vector containing OTUs to be excluded, prior to trimming. This could be obtained from extracting a subset of OTUs from the result of \code{\link{load_tax}}.
 #' @param thresh Threshold used to determine whether any OTUs should be trimmed from the dataset. If an OTU does not reach \code{thresh} at least once in a sample, the OTU will be removed. Default threshold is 0.1.
-#' @return Returns a matrix containing OTU counts, trimmed to remove OTUs < 0.1 \% of the population.
+#' @return Returns a matrix containing OTU counts, (optionally) without OTUs contained in \code{otu_vec} and also trimmed to remove OTUs that did not cross the specified percentage in at least one sample.
 #' @export
 #' @examples
 #' load_shared("miseq.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.opti_mcc.shared", thresh = .05)
 
-load_shared <- function(shared, thresh = 0.1) {
+load_shared <- function(shared, otu_vec = NULL, thresh = 0.1) {
   # Read in OTU counts
   otu_raw <- read.table(shared, row.names = 2, header = T)
 
   # Label and numOtus columns contain the same info for all rows and should be removed
   otu_trim <- otu_raw[, -c(1:2)]
+
+  # Remove any specified OTUs, such as those corresponding to Bacteria_unclassified
+  if(!is.null(otu_vec)) {
+    otu_trim <- dplyr::select(otu_trim, -otu_vec)
+  }
 
   # Convert trim to matrix in order to work with which() since data.frame will throw an error
   otu_trim <- as.matrix(otu_trim)
