00054a5d56f68704104dc6bc676dbfa84c86d42f
Christopher Brown <christopherabrown42@gmail.com>
2019-09-24 08:55:51 -0400

    Update load_shared()

--- R/load_shared.R
+++ R/load_shared.R
@@ -1,11 +1,11 @@
 #' Reads in a file containing OTU count data and keeps only OTUs > 0.1 \% of the population.
 #'
 #' @param shared An opti_mcc.shared file resulting from processing via mothur.
-#' @param thresh Threshold used to determine whether any OTUs should be trimmed. Defaults to 0.1.
+#' @param thresh Threshold used to determine whether any OTUs should be trimmed from the dataset. If an OTU does not reach \code{thresh} at least once in a sample, the OTU will be removed. Default threshold is 0.1.
 #' @return Returns a matrix containing OTU counts, trimmed to remove OTUs < 0.1 \% of the population.
 #' @export
 #' @examples
-#' load_shared("miseq.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.opti_mcc.shared")
+#' load_shared("miseq.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.opti_mcc.shared", thresh = .05)
 
 load_shared <- function(shared, thresh = 0.1) {
   # Read in OTU counts
