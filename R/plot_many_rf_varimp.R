9c1bdf61bc480f49a56aa7980b7df8b3409d767a
Christopher Brown <christopherabrown42@gmail.com>
2019-10-10 09:10:12 -0400

    Update plot_many_rf_varimp()
    Add support for labeling OTUs when features are a mix of OTUs and other data.

--- R/plot_many_rf_varimp.R
+++ R/plot_many_rf_varimp.R
@@ -1,13 +1,14 @@
 #' Using a list of random forest models, create ordered boxplots of variable importance. The plot title is created dynamically from model attributes. This function is designed to be used in conjunction with \code{\link{many_rf}}.
 #'
-#' @param rflist A list containing the random forest models.
+#' @param rf_list A list containing the random forest models.
 #' @param tax Taxonomy table. If \code{NULL}, no taxonomic labels will be added.
-#' @return A boxplot based on \code{rflist} and \code{tax}.
+#' @param n Number of features to display in the resulting plot.
+#' @return A boxplot based on \code{rf_list} and \code{tax}.
 #' @export
 #' @examples
-#' plot_many_rf_varimp(rflist = rf_vfd_otu_100, tax = otu_good_taxonomy)
+#' plot_many_rf_varimp(rf_list = rf_vfd_otu_100, tax = otu_good_taxonomy)
 
-plot_many_rf_varimp <- function(rf_list, tax = NULL) {
+plot_many_rf_varimp <- function(rf_list, tax = NULL, n = 50) {
   mod_name <- rlang::ensym(rf_list)
 
   # extract importance metrics -- use %IncMSE only (gini/node purity can be prone to bias)
@@ -23,9 +24,9 @@ plot_many_rf_varimp <- function(rf_list, tax = NULL) {
     stop("Model type not recognized. Use a classification or regression model.")
   }
 
-  # top 50 taxa by mean IncMSE
-  top_tax <- tmp %>% dplyr::group_by(Taxon) %>% dplyr::summarize(mean_err = mean(tmp[,2])) %>% dplyr::arrange(desc(mean_err)) %>% head(50)
-  # keep only top 50 taxa arranged by mean inc_mse
+  # top n taxa by mean IncMSE
+  top_tax <- tmp %>% dplyr::group_by(Taxon) %>% dplyr::summarize(mean_err = mean(tmp[,2])) %>% dplyr::arrange(desc(mean_err)) %>% head(n)
+  # keep only top n taxa arranged by mean inc_mse
   tmp <- dplyr::filter(tmp, Taxon %in% top_tax$Taxon) %>% droplevels() # remove unused levels
 
   # reorder levels by mean
@@ -33,7 +34,9 @@ plot_many_rf_varimp <- function(rf_list, tax = NULL) {
 
   # use paste_tax() to relabel Taxon levels (designed for use at Genus/OTU  level)
   if(!is.null(tax)) {
-    levels(tmp$Taxon) <- cbmbtools::paste_tax(levels(tmp$Taxon), tax)
+    # levels(tmp$Taxon) <- cbmbtools::paste_tax(levels(tmp$Taxon), tax)
+    levs <- levels(tmp$Taxon)
+    levels(tmp$Taxon)[stringr::str_which(levs, "Otu")] <- levels(cbmbtools::paste_tax(levs[stringr::str_which(levs, "Otu")], tax))
   }
 
   meas <- names(tmp)[2]
